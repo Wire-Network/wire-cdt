d7c36f736eea76e6ebeedec7f0d5615f96c3080f
Huang-Ming Huang <huangming.huang@gmail.com>
2026-05-26 17:36:17 -0500

    Skip unsupported dotnet NuGet API key command

--- scripts/build-with-github-vcpkg-cache.sh
+++ scripts/build-with-github-vcpkg-cache.sh
@@ -311,7 +311,6 @@ else
       --password "$GITHUB_TOKEN" \
       --store-password-in-clear-text \
       --valid-authentication-types basic
-    dotnet nuget setapikey "$GITHUB_TOKEN" --source "$VCPKG_NUGET_FEED"
   elif [[ "$WIRE_CDT_NUGET_TOOL" == "mono" ]]; then
     if [[ "$(uname -s)" == "Darwin" ]]; then
       require_command mono "Install dotnet or Mono with Homebrew:
